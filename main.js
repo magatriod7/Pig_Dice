33b17342460bc0dfcff5345864763b53665cd771
JH <magatriod7@naver.com>
2021-11-25 19:32:46 +0900

    feat: making random func

---
+++ main.js
@@ -0,0 +1,16 @@
+var A_score = 0;
+var B_score = 0;
+var Random_Dice = 0;
+var Turn_cnt = 0;
+
+console.log("testing")
+
+function Random() {
+	Random_Dice = Math.floor((Math.random())*8);
+	console.log(Random_Dice);
+	Random_Dice = 0;
+
+}
+
+
+

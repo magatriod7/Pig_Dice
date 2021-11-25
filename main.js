b699d55dd707351b78a67fee2da48539d4d39377
JH <magatriod7@naver.com>
2021-11-25 19:57:14 +0900

    feat: done all

--- main.js
+++ main.js
@@ -6,15 +6,55 @@ var Turn_cnt = 0;
 console.log("testing")
 
 function Random() {
-	Random_Dice = Math.floor((Math.random())*8);
-  
+	const resultElement1 = document.getElementById('score2');	
 	const resultElement = document.getElementById('score1');
+	Random_Dice = Math.floor((Math.random())*8);
+  	if(Random_Dice != 1){
   
   	let number = resultElement.innerText;
-
 	number = parseInt(number) + Random_Dice
 
   	resultElement.innerText = number;
+	}
+	else{
+		resultElement.innerText = "You lose"
+
+		resultElement1.innerText = "You win"
+
+	}
+
+
+}
+
+function Random1() {
+
+	const resultElement = document.getElementById('score2');
+	const resultElement1 = document.getElementById('score1');
+	Random_Dice = Math.floor((Math.random())*8);
+  	if(Random_Dice != 1){
+  
+  	let number = resultElement.innerText;
+	number = parseInt(number) + Random_Dice
+
+  	resultElement.innerText = number;
+	}
+	else{
+		resultElement.innerText = "You lose"
+		resultElement1.innerText = "You win"
+
+	}
+
+
+
+}
+
+function init() {
+		const resultElement = document.getElementById('score2');
+		const resultElement1 = document.getElementById('score1');
+		resultElement.innerText = 0
+		resultElement1.innerText = 0
+
+
 }
 
 
